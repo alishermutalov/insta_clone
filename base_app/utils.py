399b3079360c79b203133a3e5df9d3d707ed34da
alishermutalov <96579968+alishermutalov@users.noreply.github.com>
2024-09-17 05:18:47 +0500

    added phonenumbers package to check -  is phone_number valid

--- base_app/utils.py
+++ base_app/utils.py
@@ -1,6 +1,8 @@
 import re
-from rest_framework.exceptions import ValidationError
 import threading
+import phonenumbers
+
+from rest_framework.exceptions import ValidationError
 from django.core.mail import send_mail
 from django.conf import settings
 
@@ -9,9 +11,10 @@ email_regex = re.compile(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
 phone_regex = re.compile(r'^\+?[1-9]\d{6,14}$')
 
 def check_email_or_phone(email_or_phone):
+    phone_number = phonenumbers.parse(email_or_phone)
     if re.fullmatch(email_regex, email_or_phone):
         return "email"
-    elif re.fullmatch(phone_regex, email_or_phone):
+    elif phonenumbers.is_valid_number(phone_number):
         return "phone"
     else:
         raise ValidationError({
